aad37b9ca5fd3177754b4a8e057b99c3993ea481
Dennys2894 <127262865+Dennys2894@users.noreply.github.com>
2023-05-07 19:58:24 -0400

    Add files via upload

---
+++ contacto.js
@@ -0,0 +1,14 @@
+$('#formulario').submit(function(event) {
+    event.preventDefault();
+
+    const nombre = $('#nombre').val();
+    const aellido = $('#apellido').val();
+    const email = $('#email').val();
+    const telefono = $('#telefono').val();
+
+    
+    const qrUrl = `https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=BEGIN%3AVCARD%0AVERSION%3A3.0%0AN%3A${nombre}%0Aapellido%3A${aellido}%0AEMAIL%3A${email}%0ATEL%3A${telefono}%0AEND%3AVCARD`;
+
+    
+    $('#qr-code').html(`<img src="${qrUrl}" alt="QR code">`);
+  });
